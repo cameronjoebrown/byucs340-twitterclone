588fd331d85ce50dff0c27c4e6c79377cd64f76b
Cameron Brown <cameronjoebrown@gmail.com>
2020-10-14 02:25:44 -0600

    Setup BeforeEach for LoginServiceTest

--- app/src/test/java/edu/byu/cs/tweeter/model/service/LoginServiceTest.java
+++ app/src/test/java/edu/byu/cs/tweeter/model/service/LoginServiceTest.java
@@ -1,7 +1,11 @@
 package edu.byu.cs.tweeter.model.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.ServerFacade;
 import edu.byu.cs.tweeter.model.service.request.LoginRequest;
 import edu.byu.cs.tweeter.model.service.response.LoginRegisterResponse;
 
@@ -13,7 +17,7 @@ public class LoginServiceTest {
     private LoginRegisterResponse successResponse;
     private LoginRegisterResponse failureResponse;
 
-    private LoginService LoginServiceSpy;
+    private LoginService loginServiceSpy;
 
     /**
      * Create a LoginService spy that uses a mock ServerFacade to return known responses to
@@ -21,6 +25,23 @@ public class LoginServiceTest {
      */
     @BeforeEach
     public void setup() {
+        // Setup request objects to use in the tests
+        User user1 = new User("Bob", "Joe", "@bobjoe",
+                "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png");
+        validRequest = new LoginRequest(user1.getUsername(), "Iamcool");
+        invalidRequest = new LoginRequest(null, null);
+
+        // Setup a mock ServerFacade that will return known responses
+        successResponse = new LoginRegisterResponse(user1, new AuthToken());
+        ServerFacade mockServerFacade = Mockito.mock(ServerFacade.class);
+        Mockito.when(mockServerFacade.login(validRequest)).thenReturn(successResponse);
+
+        failureResponse = new LoginRegisterResponse("An exception occured");
+        Mockito.when(mockServerFacade.login(invalidRequest)).thenReturn(failureResponse);
+
+        // Create a LoginService instance and wrap it with a spy that will use the mock service
+        loginServiceSpy = Mockito.spy(new LoginService());
+        Mockito.when(loginServiceSpy.getServerFacade()).thenReturn(mockServerFacade);
 
     }
 }
