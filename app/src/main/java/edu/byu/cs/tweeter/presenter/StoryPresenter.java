f782a021b406bb591da0cd0bf53413a0a355b142
Cameron Brown <cameronjoebrown@gmail.com>
2020-10-15 16:04:48 -0600

    Started Presenter classes

---
+++ app/src/main/java/edu/byu/cs/tweeter/presenter/StoryPresenter.java
@@ -0,0 +1,39 @@
+package edu.byu.cs.tweeter.presenter;
+
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.model.service.StoryService;
+import edu.byu.cs.tweeter.model.service.request.FeedStoryRequest;
+import edu.byu.cs.tweeter.model.service.response.FeedStoryResponse;
+
+/**
+ * The presenter for the story functionality of the application.
+ */
+public class StoryPresenter {
+    private final View view;
+    /**
+     * The interface by which this presenter communicates with it's view.
+     */
+    public interface View {
+        // If needed, specify methods here that will be called on the view in response to model updates
+    }
+
+    /**
+     * Creates an instance.
+     *
+     * @param view the view for which this class is the presenter.
+     */
+    public StoryPresenter(View view) {
+        this.view = view;
+    }
+
+    /**
+     * Makes a Get Story Request
+     *
+     * @param request the story request
+     */
+    public FeedStoryResponse getStory(FeedStoryRequest request) throws IOException {
+        StoryService storyService = new StoryService();
+        return storyService.getStory(request);
+    }
+}
