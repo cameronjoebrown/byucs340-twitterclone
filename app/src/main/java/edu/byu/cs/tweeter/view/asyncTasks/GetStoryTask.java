83450dc2bf01ee9e6ebe3de620802246572383f2
Cameron Brown <cameronjoebrown@gmail.com>
2020-10-20 02:35:34 -0600

    Finished all of the AsyncTasks

---
+++ app/src/main/java/edu/byu/cs/tweeter/view/asyncTasks/GetStoryTask.java
@@ -0,0 +1,77 @@
+package edu.byu.cs.tweeter.view.asyncTasks;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.model.service.request.FeedStoryRequest;
+import edu.byu.cs.tweeter.model.service.response.FeedStoryResponse;
+import edu.byu.cs.tweeter.presenter.StoryPresenter;
+
+/**
+ * An {@link AsyncTask} for retrieving story for a user.
+ */
+public class GetStoryTask extends AsyncTask<FeedStoryRequest, Void, FeedStoryResponse> {
+    private final StoryPresenter presenter;
+    private final Observer observer;
+    private Exception exception;
+
+    /**
+     * An observer interface to be implemented by observers who want to be notified when this task
+     * completes.
+     */
+    public interface Observer {
+        void storyRetrieved(FeedStoryResponse response);
+        void handleException(Exception exception);
+    }
+
+    /**
+     * Creates an instance.
+     *
+     * @param presenter the presenter from whom this task should retrieve statuses.
+     * @param observer the observer who wants to be notified when this task completes.
+     */
+    public GetStoryTask(StoryPresenter presenter, Observer observer) {
+        if(observer == null) {
+            throw new NullPointerException();
+        }
+
+        this.presenter = presenter;
+        this.observer = observer;
+    }
+
+    /**
+     * The method that is invoked on the background thread to retrieve statuses. This method is
+     * invoked indirectly by calling {@link #execute(FeedStoryRequest...)}.
+     *
+     * @param storyRequests the request object (there will only be one).
+     * @return the response.
+     */
+    @Override
+    protected FeedStoryResponse doInBackground(FeedStoryRequest... storyRequests) {
+
+        FeedStoryResponse response = null;
+
+        try {
+            response = presenter.getStory(storyRequests[0]);
+        } catch (IOException ex) {
+            exception = ex;
+        }
+
+        return response;
+    }
+
+    /**
+     * Notifies the observer (on the UI thread) when the task completes.
+     *
+     * @param storyResponse the response that was received by the task.
+     */
+    @Override
+    protected void onPostExecute(FeedStoryResponse storyResponse) {
+        if(exception != null) {
+            observer.handleException(exception);
+        } else {
+            observer.storyRetrieved(storyResponse);
+        }
+    }
+}
